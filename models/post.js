0ab30b2c548929e49f0bc4837db54a8ab9abdb03
nataila <nataila.cc.814@gmail.com>
2016-01-19 10:22:51 +0800

    博文存入

---
+++ models/post.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nodeBlog');
+
+var PostSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  created_at: {type: Date, default: Date.now}
+}, {
+  collection: 'post'
+});
+
+var PostModel = mongoose.model('Post', PostSchema);
+
+function Post(post) {
+  this.title = post.title;
+  this.content = post.content;
+  this.created_at = post.created_at;
+}
+
+Post.prototype.save = function (callback) {
+  var post = {
+    title: this.title,
+    content: this.content,
+    created_at: this.created_at
+  };
+  var newPost = new PostModel(post);
+  newPost.save(function (err, post) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, post);
+  });
+};
+
+module.exports = Post;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 /* GET home page. */
 var express = require('express');
 var bodyParser = require('body-parser');
-var db = require('../models/db');
+var Post = require('../models/post');
 module.exports = function (app) {
   app.get('/', function(req, res) {
     res.render('index', {'title': 'Express'});
@@ -13,5 +13,12 @@ module.exports = function (app) {
   app.post('/post', function (req, res) {
     var title = req.body.title;
     var content = req.body.content;
+    var post = new Post({
+      title: title,
+      content: content
+    });
+    post.save(function (err, post) {
+      console.log(post);
+    });
   });
 };
